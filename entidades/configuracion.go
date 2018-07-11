e509e5cf9606a9cba2d7896ade3a81269263d520
tonismr1 <40060577+tonismr1@users.noreply.github.com>
2018-07-11 13:54:48 +0200

    Estructura Base

---
+++ entidades/configuracion.go
@@ -0,0 +1,10 @@
+package entidades
+
+type Configuracion struct {
+	Url      string
+	Port     int
+	User     string
+	Password string
+}
+
+ 
---
+++ main.go
@@ -0,0 +1,13 @@
+
+package main
+
+import (
+	"fmt"
+	"github.com/everis/jira/Entidades"
+)
+
+func main() {
+
+	fmt.Println(entidades.Configuracion{"localhost", 1994, "admin", "admin"})
+
+}
